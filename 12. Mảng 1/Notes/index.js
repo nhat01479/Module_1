5fd465daa531602dc082cd982d43ed0778ae7a51
nhat01479 <nhat01479@gmail.com>
2023-03-21 17:23:26 +0700

    a

---
+++ 12. Mảng 1/Notes/index.js
@@ -0,0 +1,51 @@
+// let a = [3,5,6,8,8,1];
+// let sum=0;
+// for ( let i=a.length - 3; i < a.length; i++) {
+//      sum += a[i];
+// }
+// console.log(sum);
+
+// let a = [3,3,6,8,1];
+// let max = a[0];
+// for (i=0; i<a.length; i++) {
+//     if (a[i] > max) max = a[i];
+// }
+// console.log(max);
+//-----------------------------------------------------
+// let a = [3,3,6,8,1];
+// let max = a[0];
+// let p;
+// for (i=0; i<a.length; i++) {            //Tìm số lớn nhất
+//     if (a[i] > max) {
+//         max = a[i];
+//         p = i;
+//     }
+// }
+// console.log(max);
+// console.log(p);
+//------------------------------------------------
+
+
+// let a = [3,6,8,1,7];
+// for (i = 0; i < a.length/2; i++) {
+//     let temp = a[i];                    //Đảo ngược mạng
+//     a[i]=a[a.length - 1 -i];
+//     a[a.length - 1 - i] = temp;
+//     }
+// console.log(a);
+
+// let arr = [
+//     [2,1,7,5],
+//     [1,1,3,3],
+//     [2,2,1,8],
+//     [2,5,1,6]
+// ];
+// let str='';
+// for (let i=0; i<arr.length; i++) {
+//     for (let j=0; j<arr.length; j++) {
+//         str += arr[i][j]
+//     }
+//     str += '\n';
+// }
+// console.log(str);
+
